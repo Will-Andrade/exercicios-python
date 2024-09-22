a70e0f66540fd417411b8273f58be4db2c39b301
Will-Andrade <willian.santos.andra@outlook.com>
2024-09-22 00:52:20 -0400

    feat: first python exercise done

---
+++ arrays.py
@@ -0,0 +1,15 @@
+'''
+Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal 
+usando o comando print(). O primeiro array deve conter os produtos de uma loja da sua
+escolha (loja de comida, materiais de construção, música, etc). O segundo array deve
+conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes
+descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada
+lista (strings, booleanos, números inteiros, floats).
+'''
+
+produtos_cafeteria: list[str] = ["Salgado", "Coca-Cola 2L", "Refrigerante lata", "Baconzitos", "Bolacha Mabel"]
+
+anos_nascimento: list[int] = [1999, 2019, 2001, 1972, 1995]
+
+print("Produtos da Cafeteria", produtos_cafeteria)
+print("Anos de nascimento", anos_nascimento)
